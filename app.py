b9d96f8c22ae6da5b738feb224bbe84f3b9a0ee8
yuanxiaoms <87499897+yuanxiaoms@users.noreply.github.com>
2021-07-29 11:52:39 -0700

    Update app.py
    Add keras predict

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
+from tensorflow import keras
+import tensorflow as tf
 
 app = Flask(__name__)
 
@@ -12,7 +14,17 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    
+      image_size = (180, 180)
+      img = keras.preprocessing.image.load_img("PetImages/Cat/6779.jpg", target_size=image_size)
+      img_array = keras.preprocessing.image.img_to_array(img)
+      img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+      model = keras.models.load_model('cat_dog')
+      predictions = model.predict(img_array)
+      score = predictions[0]
+      ret = "This image is %.2f percent cat and %.2f percent dog." % (100 * (1 - score), 100 * score))
+      return 'file uploaded successfully ' + ret
 
 if __name__ == '__main__':
     app.run(debug=True)
